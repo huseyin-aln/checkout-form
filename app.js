75e863393b88018f26bf48c7af4e5500ff1d0262
huseyin <huseyinarslan06@gmail.com>
2022-06-04 00:04:57 +0300

    with buttons product amount is changed

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const shippingPrice = 19.0;
 let productsDiv = document.querySelector(".products-panel");
 
 productsDiv.addEventListener("click", (event) => {
-    if (event.target.className == "fas fa-minus" ) {
-        console.log(event.target.nextElementSibling.innerText;); 
+    if (event.target.classList.contains("fa-minus")) {
+        if (event.target.closest(".quantity-controller").querySelector("p").innerText > 0) {
+            event.target.closest(".quantity-controller").querySelector("p").innerText--;
+        } else {
+            if (confirm("Product will be deleted?")) {
+                event.target.parentElement.parentElement.parentElement.parentElement.remove();
+            }
+        }
     }
-}
+    else if (event.target.classList.contains("fa-plus")) {
+        event.target.closest(".quantity-controller").querySelector("p").innerText++;
+    }
+});
+
+//* calculate cart and product totals 
